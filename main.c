7d2498966a0fd473cd9bedb61d5fbb4e4fa26726
Seb M <jgistgaming17@gmail.com>
2023-09-05 12:09:39 -0400

    cleaning up
    debloating and cleaning up all of the bloat comments  that raylib puts in the default example

--- main.c
+++ main.c
@@ -1,30 +1,24 @@
 #include "include/raylib.h"
+#include "stdio.h"
+#include "math.h"
+#include "stdint.h"
+#include "string.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);               // 60 fps frame-lock
 
-    // Main game loop
+    //window loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // variables
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        // draw loop
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
@@ -32,13 +26,10 @@ int main(void)
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    if(IsKeyDown(KEY_ESCAPE))
+        CloseWindow();
 
     return 0;
 }
